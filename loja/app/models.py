d3c1d9c2e3a570f9e0bb1a3efb0141378eeb3a73
monealc <111471021+monealc@users.noreply.github.com>
2023-08-31 14:54:37 -0300

    Add files via upload

---
+++ loja/app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome_categoria = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.nome_categoria
+    
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.nome
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca,on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    preco = models.PositiveIntegerField()
+    disponibilidade = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Categoria: {self.categoria}, Marca: {self.marca} "
